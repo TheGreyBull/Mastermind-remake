af7e03d6bbad2d1ad33a9e1469fe4e53ded105c0
TheGreyBull <thegreybull@outlook.com>
2023-08-10 09:44:58 +0200

    Finished the okOption method and added all colors

--- src/OptionPane.java
+++ src/OptionPane.java
@@ -1,23 +1,55 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.font.FontRenderContext;
+import java.awt.font.LineBreakMeasurer;
+import java.awt.font.TextAttribute;
+import java.text.AttributedCharacterIterator;
+import java.text.AttributedString;
 
 public class OptionPane extends JFrame {
     JTextArea messageArea;
     JPanel buttonsPanel;
+    // All dark theme colors
+    Color darkBackground;
+    Color darkButtonBackground;
+    Color darkButtonText;
+    // All light theme colors
+    Color lightBackground;
+    Color lightButtonBackground;
+    Color lightButtonText;
+
     protected OptionPane() {
         defaultSettings();
     }
 
     protected void okOption(boolean isDarkTheme, String title, String message) {
-        reUsableSettings(isDarkTheme);
+        reUsableSettings();
         this.setTitle(title);
         messageArea.setText(message);
         JButton okButton = new JButton("OK");
-        buttonSettings(isDarkTheme, okButton);
+        buttonSettings(okButton);
+        textAreaResize();
+        okButton.addActionListener(e -> this.dispose());
+        if (isDarkTheme) {
+            this.getContentPane().setBackground(darkBackground);
+            messageArea.setBackground(darkBackground);
+            messageArea.setForeground(Color.WHITE);
+            buttonsPanel.setBackground(darkBackground);
+            okButton.setBackground(darkButtonBackground);
+            okButton.setForeground(darkButtonText);
+        }
+        else {
+            this.getContentPane().setBackground(lightBackground);
+            messageArea.setBackground(lightBackground);
+            messageArea.setForeground(lightButtonText);
+            buttonsPanel.setBackground(lightBackground);
+            okButton.setBackground(lightButtonBackground);
+            okButton.setForeground(lightButtonText);
+        }
     }
 
     protected int multiOption(boolean isDarkTheme, String title, String message, String[] choices) {
-        reUsableSettings(isDarkTheme);
+        reUsableSettings();
         /*new WindowAdapter() {
             public void windowClosing(WindowEvent evt) {
                 Frame frame = (Frame) evt.getSource();
@@ -47,19 +79,66 @@ public class OptionPane extends JFrame {
 
         this.getContentPane().add(messageArea);
         this.getContentPane().add(buttonsPanel);
+
+        // Declaration
+        darkBackground = new Color(0x172A46);
+        darkButtonBackground = new Color(0x0A192F);
+        darkButtonText = new Color(0xBAC5E6);
+
+        lightBackground = new Color(0xDDE1FA);
+        lightButtonBackground = new Color(0xF1F1FB);
+        lightButtonText = new Color(0x523EB9);
     }
-    private void reUsableSettings(boolean isDarkTheme) {
+    private void reUsableSettings() {
         messageArea.setText("");
         buttonsPanel.removeAll();
         this.setLocationRelativeTo(null);
         this.setVisible(true);
+        this.setSize(450, 225);
         messageArea.setBounds(20, 20, 400, 115);
-        messageArea.setBorder(BorderFactory.createLineBorder(Color.BLACK, 1));
+        buttonsPanel.setBounds(0, 145, 450, 45);
     }
 
-    private void buttonSettings(boolean isDarkTheme, JButton button) {
+    private void buttonSettings(JButton button) {
         buttonsPanel.add(button);
         button.setHorizontalAlignment(SwingConstants.CENTER);
         button.setFocusable(false);
+    }
+
+    private void textAreaResize() {
+        System.out.println(countLines(messageArea));
+        int linesCount = countLines(messageArea);
+        linesCount += (messageArea.getLineCount() - 1);
+        /*
+         * Default frame size supports up to 7 lines length
+         * Increases by 15 for each line
+         */
+        if (linesCount > 7) {
+            linesCount -= 7;
+            this.setSize(450, 225 + (linesCount * 15));
+            messageArea.setBounds(20, 20, 400, 115 + (linesCount * 15));
+            buttonsPanel.setBounds(0, 145 + (linesCount * 15), 450, 45);
+            this.setLocationRelativeTo(null);
+        }
+    }
+
+    private int countLines(JTextArea textArea) {
+        AttributedString text = new AttributedString(textArea.getText());
+        text.addAttribute(TextAttribute.FONT, textArea.getFont());
+        FontRenderContext frc = textArea.getFontMetrics(textArea.getFont()).getFontRenderContext();
+        AttributedCharacterIterator charIt = text.getIterator();
+        LineBreakMeasurer lineMeasurer = new LineBreakMeasurer(charIt, frc);
+        Insets textAreaInsets = textArea.getInsets();
+        float formatWidth = textArea.getWidth() - textAreaInsets.left - textAreaInsets.right;
+        lineMeasurer.setPosition(charIt.getBeginIndex());
+
+        int noLines = 0;
+        while (lineMeasurer.getPosition() < charIt.getEndIndex())
+        {
+            lineMeasurer.nextLayout(formatWidth);
+            noLines++;
+        }
+
+        return noLines;
     }
 }
