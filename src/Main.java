e52eb51c130c07b894c971ecabe9fdc10eb797fe
TheGreyBull <thegreybull@outlook.com>
2023-08-14 12:22:44 +0200

    Added CirclePanel class and fixed some loading issues

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        OptionPane option = new OptionPane();
-        option.okOption(true, "Lorem ipsum", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce sit amet mauris ut risus scelerisque sodales pellentesque eget odio. Maecenas fringilla, dui at sollicitudin venenatis, libero justo commodo ligula, nec consequat justo nisi et libero. Pellentesque interdum blandit libero non dapibus. Interdum et malesuada fames ac ante ipsum primis in faucibus. Sed eu dapibus felis. Curabitur tellus velit, accumsan vel gravida sed, tincidunt ut est. Etiam eget dictum erat. Donec tortor dui, blandit quis porta facilisis, blandit vel mauris. Nullam venenatis urna accumsan nulla tincidunt, sit amet varius massa mollis. Nam semper lectus consectetur feugiat lobortis. Nunc gravida neque vestibulum tortor volutpat pretium vitae quis arcu. Pellentesque ullamcorper dui ligula, quis sollicitudin eros interdum quis. Proin tincidunt finibus pretium. Etiam pretium libero nulla, tempus fringilla leo sagittis eget.\n" +
-                "\n" +
-                "Aliquam sit amet enim congue, auctor felis ut, tristique sapien. Vivamus non massa eget neque cursus fringilla. Nam dignissim fermentum finibus. Interdum et malesuada fames ac ante ipsum primis in faucibus. Cras nisl arcu, rhoncus non tortor dictum, maximus placerat felis. Nulla sollicitudin commodo faucibus. Cras ullamcorper enim nec porta feugiat. Nulla at egestas est, in eleifend justo. Suspendisse quis suscipit tortor. Integer ac tristique justo. Nulla vitae ante auctor lectus cursus pellentesque a et velit. In porta tellus id ante congue, at lobortis nisl sagittis.\n" +
-                "\n" +
-                "Donec pharetra ultricies iaculis. Pellentesque elementum lorem tempor urna volutpat ultricies. Aliquam in dui nec neque dictum luctus at et ligula. In a feugiat felis, at ullamcorper elit. Curabitur at diam posuere, imperdiet quam nec, luctus arcu. Vivamus at nulla id purus tincidunt hendrerit. Vivamus blandit velit et rutrum auctor. Sed lacus odio, auctor eget tortor non, semper pharetra ipsum. Sed tempus porta risus in tincidunt.\n" +
-                "\n" +
-                "Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Maecenas dignissim leo ligula, vel sagittis lacus blandit at. Nullam arcu purus, consectetur quis congue vitae, lobortis non nunc. Praesent id tellus non turpis bibendum aliquam. Praesent a metus finibus, mollis tellus sed, sagittis quam. In mollis dictum quam tincidunt aliquam. Suspendisse eget purus feugiat, aliquam elit ut, eleifend risus.");
+        new MainGame();
     }
 }
--- src/MainGame.java
+++ src/MainGame.java
@@ -1,7 +1,64 @@
 import javax.swing.*;
+import javax.swing.border.Border;
+import java.awt.*;
 
 public class MainGame extends JFrame {
-    protected MainGame () {
+    double WIDTH = 0.7;
+    double HEIGHT = 0.685;
+    OptionPane option;
+    Color darkFrameBackground;
+    Color darkPanelBackground;
+    JPanel rowsPanel;
+    JPanel guessingPanel;
+    JPanel resultPanel;
+    JPanel colorsPanel;
 
+    protected MainGame () {
+        frameSettings();
+        initializations();
+    }
+
+    // Initializations of global variables
+    private void initializations() {
+        option = new OptionPane();
+        darkFrameBackground = new Color(0x0A192F);
+        darkPanelBackground = new Color(0x172A46);
+
+        this.getContentPane().setBackground(darkFrameBackground);
+
+        rowsPanel = new JPanel();
+        guessingPanel = new JPanel();
+        resultPanel = new JPanel();
+        colorsPanel = new JPanel();
+
+        rowsPanel.setBackground(darkPanelBackground);
+        guessingPanel.setBackground(darkPanelBackground);
+        resultPanel.setBackground(darkPanelBackground);
+        colorsPanel.setBackground(darkPanelBackground);
+
+        // Adding every component
+        this.add(rowsPanel);
+        this.add(guessingPanel);
+        this.add(resultPanel);
+        this.add(colorsPanel);
+
+        resizeVariables();
+    }
+
+    private void frameSettings() {
+        this.setResizable(true);
+        this.setTitle("Mastermind");
+        this.setSize(1200, 700);
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.setExtendedState(JFrame.MAXIMIZED_BOTH);
+        this.setVisible(true);
+        this.setLayout(null);
+    }
+
+    private void resizeVariables() {
+        rowsPanel.setBounds((int) (WIDTH * 280), (int) (HEIGHT * 26), (int) (WIDTH * 100), (int) (HEIGHT * 1028));
+        guessingPanel.setBounds((int) (WIDTH * 400), (int) (HEIGHT * 26), (int) (WIDTH * 800), (int) (HEIGHT * 1028));
+        resultPanel.setBounds((int) (WIDTH * 1220), (int) (HEIGHT * 26), (int) (WIDTH * 250), (int) (HEIGHT * 1028));
+        colorsPanel.setBounds((int) (WIDTH * 1490), (int) (HEIGHT * 26), (int) (WIDTH * 150), (int) (HEIGHT * 1028));
     }
 }
